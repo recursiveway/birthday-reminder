ebffbb026a6063df72768f7f568cb627972702b3
Ashutosh Choudhary <ashutoshchoudhary998@gmail.com>
2023-01-24 13:01:25 +0530

    dome

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { useState } from 'react';
 import data from './data';
 import List from './List';
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data)
+  return <main>
+    <section className='container'>
+      <h3>5 birthday today</h3>
+      <List datas={people} />
+      <button onClick={() => setPeople([])}>clear all</button>
+    </section>
+  </main>
 }
 
 export default App;
--- src/List.js
+++ src/List.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
-const List = () => {
+const List = ({ datas }) => {
+  const renderedList = datas.map((data) => {
+    return (<article key={data.id} className='person'>
+      <img src={data.image} alt={data.name} />
+      <div>
+        <h4>{data.name}</h4>
+        <p>{data.age}</p>
+      </div>
+    </article>)
+  })
   return (
-    <>
-      <h2>list component</h2>
-    </>
+    <ul>
+      {renderedList}
+    </ul>
   );
 };
 
